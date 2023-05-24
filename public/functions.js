6b67cb886c6107971e40c0bbcf059bb19e59910c
PaulV <40007694+legitcat@users.noreply.github.com>
2023-05-24 15:28:03 -0700

    Add files via upload

--- public/functions.js
+++ public/functions.js
@@ -17,14 +17,26 @@ function accordian(id){
           document.getElementById("panel1").style.display="block";
           document.getElementById("accordian2").style.backgroundColor = "#eee"; 
           document.getElementById("panel2").style.display="";
+          document.getElementById("accordian3").style.backgroundColor = "#eee"; 
+          document.getElementById("panel3").style.display="";
       }
       else if (id=="2" && document.getElementById("panel2").style.display==""){
           document.getElementById("accordian2").style.backgroundColor = "orange"; 
           document.getElementById("panel2").style.display="block";
           document.getElementById("accordian1").style.backgroundColor = "#eee"; 
           document.getElementById("panel1").style.display="";
+          document.getElementById("accordian3").style.backgroundColor = "#eee"; 
+          document.getElementById("panel3").style.display="";
       }
-      else if (id=="1" && document.getElementById("panel1").style.display=="block"||id=="2" && document.getElementById("panel2").style.display=="block"){
+      else if (id=="3" && document.getElementById("panel3").style.display==""){
+          document.getElementById("accordian3").style.backgroundColor = "orange"; 
+          document.getElementById("panel3").style.display="block";
+          document.getElementById("accordian1").style.backgroundColor = "#eee"; 
+          document.getElementById("panel1").style.display="";
+          document.getElementById("accordian2").style.backgroundColor = "#eee"; 
+          document.getElementById("panel2").style.display="";
+      }
+      else if ((id=="1" && document.getElementById("panel1").style.display=="block")||(id=="2" && document.getElementById("panel2").style.display=="block")||(id=="3" && document.getElementById("panel3").style.display=="block")){
 
           for (let i = 1; 3 < 4; i++) {
              document.getElementById("accordian"+i).style.backgroundColor = "#eee"; 
