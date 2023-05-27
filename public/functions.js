3c54760916e90b1bdf84b814dc3d3ae0d373fdb6
PaulV <40007694+legitcat@users.noreply.github.com>
2023-05-27 15:01:10 -0700

    Add files via upload

--- public/functions.js
+++ public/functions.js
@@ -11,39 +11,17 @@ function closeNav() {
 
 function accordian(id){ 
 
+      for (let i = 1; i < 4; i++) {
+             if (id==i && document.getElementById("panel"+i).style.display==""){
+                   document.getElementById("accordian"+i).style.backgroundColor = "orange"; 
+                   document.getElementById("panel"+i).style.display="block"; 
+                }
+             else{
+                   document.getElementById("accordian"+i).style.backgroundColor = "#F0F0F0"; 
+                   document.getElementById("panel"+i).style.display=""; 
+                }
 
-      if (id=="1" && document.getElementById("panel1").style.display==""){
-          document.getElementById("accordian1").style.backgroundColor = "orange"; 
-          document.getElementById("panel1").style.display="block";
-          document.getElementById("accordian2").style.backgroundColor = "#eee"; 
-          document.getElementById("panel2").style.display="";
-          document.getElementById("accordian3").style.backgroundColor = "#eee"; 
-          document.getElementById("panel3").style.display="";
-      }
-      else if (id=="2" && document.getElementById("panel2").style.display==""){
-          document.getElementById("accordian2").style.backgroundColor = "orange"; 
-          document.getElementById("panel2").style.display="block";
-          document.getElementById("accordian1").style.backgroundColor = "#eee"; 
-          document.getElementById("panel1").style.display="";
-          document.getElementById("accordian3").style.backgroundColor = "#eee"; 
-          document.getElementById("panel3").style.display="";
-      }
-      else if (id=="3" && document.getElementById("panel3").style.display==""){
-          document.getElementById("accordian3").style.backgroundColor = "orange"; 
-          document.getElementById("panel3").style.display="block";
-          document.getElementById("accordian1").style.backgroundColor = "#eee"; 
-          document.getElementById("panel1").style.display="";
-          document.getElementById("accordian2").style.backgroundColor = "#eee"; 
-          document.getElementById("panel2").style.display="";
-      }
-      else if ((id=="1" && document.getElementById("panel1").style.display=="block")||(id=="2" && document.getElementById("panel2").style.display=="block")||(id=="3" && document.getElementById("panel3").style.display=="block")){
-
-          for (let i = 1; 3 < 4; i++) {
-             document.getElementById("accordian"+i).style.backgroundColor = "#eee"; 
-             document.getElementById("panel"+i).style.display="";            
-          }
-
-      }   
+        }   
 
    }
 
